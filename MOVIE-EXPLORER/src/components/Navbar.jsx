4c1760ae07a782c307c4fe7d8d88d0475fe39640
Padma <padmasgithubaccoun@gmail.com>
2026-03-15 09:31:21 +0530

    Movie-Explorer app

---
+++ MOVIE-EXPLORER/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+         <nav className="navbar">
+		  <Link className="logo" to="/">
+			<h2>🎬 Movie Explorer</h2>
+		  </Link>
+		  <Link to="/">Home</Link>
+	     </nav>
+    </div>
+  )
+}
+
+export default Navbar
